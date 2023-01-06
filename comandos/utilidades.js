ddb106da907ed4a54d811d8ea48cfc990e3c865b
Lucas Pelosi <62014653+LucasPelosi21@users.noreply.github.com>
2023-01-05 22:52:58 -0300

    teste

--- comandos/utilidades.js
+++ comandos/utilidades.js
@@ -18,7 +18,7 @@ module.exports = utilidades = async(client,message) => {
         const uaOverride = 'WhatsApp/2.2029.4 Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36'
 
         switch(command){   
-            case 'placa':
+            case '!placa':
                 if (args.length == 0) return await client.reply(from, 'Coloque uma placa para puxar.', id)
                 if (!isGroupMsg) return await client.reply(from, msgs_texto.permissao.grupo, id)
 
